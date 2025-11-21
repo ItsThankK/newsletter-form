bde01a9c56f252594410ae7765baa0e526ac6fd3
ItsThankK <tkingsway3@gmail.com>
2025-11-21 12:07:05 +0100

    implement email validation and error states

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ const cardWithForm = document.querySelector(".card");
 const cardSuccess = document.querySelector(".card-success");
 const dismissBtn = document.querySelector(".btn-card-success");
 const inputField = document.querySelector("input");
+const errorMessage = document.querySelector(".error-state");
+
+inputField.addEventListener("input", () => {
+  if (!validator.isEmail(inputField.value)) {
+    errorMessage.classList.remove("hidden");
+    inputField.classList.add("error");
+
+    errorMessage.textContent = "Valid email required";
+    errorMessage.style.color = "red";
+  }
+
+  if (validator.isEmail(inputField.value)) {
+    inputField.classList.remove("error");
+
+    errorMessage.textContent = "Valid email ✅";
+    errorMessage.style.color = "green";
+  }
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -16,5 +34,6 @@ dismissBtn.addEventListener("click", () => {
   cardSuccess.classList.toggle("hidden");
 
   inputField.value = "";
+  errorMessage.classList.add("hidden");
   inputField.focus();
 });
